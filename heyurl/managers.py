787a4811362c645c1fd3d8c5dc2d537ce6d8701e
Marcio Santiago <marcio704@yahoo.com.br>
2021-12-24 01:24:01 -0300

    Fixing ClickQueryset.for_current_month

--- heyurl/managers.py
+++ heyurl/managers.py
@@ -5,7 +5,10 @@ from django.db.models import QuerySet, Count
 class ClickQueryset(QuerySet):
 
     def for_current_month(self):
-        return self.filter(created_at__date__month=datetime.now().month)
+        return self.filter(
+            created_at__date__month=datetime.now().month,
+            created_at__date__year=datetime.now().year,
+        )
 
     def aggregate_by_browser(self):
         return self.values('browser').annotate(browser_count=Count('browser'))
